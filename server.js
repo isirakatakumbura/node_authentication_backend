cb5f00264838ec83b387367a372574ae4bc8a00b
Isira Katakumbura <isirakatakumburs@gmail.com>
2026-04-18 10:30:30 -0600

    added the changes to link with frontend dashboard and remove locally used html page

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+const cors = require('cors');
 const {connectDB, sequelize} = require('./config/db');
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin');
@@ -7,17 +7,22 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public', 'signup.html'));
-});
+// app.get('/', (req,res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+// });
 
 const PORT = process.env.PORT || 3000;
 
@@ -28,7 +33,7 @@ const startServer = async () => {
         console.log('Databse tables synced');
 
         app.listen(PORT, () => {
-            console.log('Server running on http://localhost:${PORT}');
+            console.log(`Server running on http://localhost:${PORT}`);
         });
     
     } catch (error) {
